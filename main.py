ed78032f27d64528db074e329d9207662e8b6144
DIAB_notebook <diab@bk.ru>
2023-07-17 17:50:03 +0300

    Реализованы действия для команд (вперед, назад, вверх, вниз). Реализован конец игры.

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 pole = [
-    ['0', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*'],
+    ['0', '*', '#', '$', '*'],
+    ['#', '*', '#', '*', '*'],
+    ['*', '*', '#', '#', '*'],
+    ['*', '#', '*', '#', '*'],
     ['*', '*', '*', '*', '*'],
 ]
 
+# Ось по вертикали
 pole_x = 0
+# Ось по горизонтали
 pole_y = 0
 command = ""
-while command != "выход":
-    for step in pole:
-        print(step)
-    command = input("Введите команду: ")
 
-    if command == "вперед":
-        pass
+for step_ryad in pole:
+    for step_elem in step_ryad:
+        print(step_elem, end=" ")
+    print()
+command_user = input("Введите команду: ")
 
-    if command == "назад":
-        pass
+while command_user != "выход":
+    if command_user == "вперед":
+        if pole_y < 4 and pole[pole_x][pole_y + 1] != "#":
+            pole[pole_x][pole_y] = "*"
+            pole_y += 1
+            pole[pole_x][pole_y] = "0"
 
-    if command == "вверх":
-        pass
+    elif command_user == "назад":
+        if pole_y > 0 and pole[pole_x][pole_y - 1] != "#":
+            pole[pole_x][pole_y] = "*"
+            pole_y -= 1
+            pole[pole_x][pole_y] = "0"
 
-    if command == "вниз":
-        pass
+    elif command_user == "вниз":
+        if pole_x < 4 and pole[pole_x + 1][pole_y] != "#":
+            pole[pole_x][pole_y] = "*"
+            pole_x += 1
+            pole[pole_x][pole_y] = "0"
+
+    elif command_user == "вверх":
+        if pole_x > 0 and pole[pole_x - 1][pole_y] != "#":
+            pole[pole_x][pole_y] = "*"
+            pole_x -= 1
+            pole[pole_x][pole_y] = "0"
 
     else:
         print("Нет такой команды!")
+
+    for step_ryad in pole:
+        for step_elem in step_ryad:
+            print(step_elem, end=" ")
+        print()
+    if pole_x == 0 and pole_y == 3:
+        print("Победа")
+        break
+    command_user = input("Введите команду: ")
 
 print("Game over!")
